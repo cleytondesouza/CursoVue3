67504f36162796d6f7b26ab2c878d54d6a5b5d1d
Cleyton Pereira de Souza <79112944+cleytondesouza@users.noreply.github.com>
2022-01-05 09:43:51 -0300

    Conclusão dos Conceitos

---
+++ conceitos/lifecycle-hooks/src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <h1>Minha Lista de Tarefas!</h1>
+
+    <button @click="handleShowHideList">Ver a Lista!</button>
+    <br />
+    <input
+      type="text"
+      @keyup.enter="addTask"
+      v-focus
+      v-model="state.currentTask"
+    />
+
+    <ul v-if="state.showList">
+      <li
+        v-for="(task, index) in state.tasks"
+        @dblclick="complete(task)"
+        :key="`${task}-${index}`"
+        class="task-item"
+        :class="{
+          linha: task.isDone,
+        }"
+      >
+        {{ task.name }}
+
+        <button @click="remove(task)">&times;</button>
+      </li>
+    </ul>
+    <p v-else>Lista de tarefas escondida</p>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+
+const focus = {
+  inserted: (el) => {
+    el.focus();
+  },
+};
+
+export default {
+  directives: {
+    focus,
+  },
+  setup() {
+    const state = reactive({
+      currentTask: "",
+      showList: false,
+      tasks: [{ name: "Fazer o curso", isDone: false }],
+    });
+
+    function handleShowHideList() {
+      state.showList = !state.showList;
+    }
+    function addTask() {
+      state.tasks.push({
+        name: state.currentTask,
+        isDone: false,
+      });
+      state.currentTask = "";
+    }
+    function complete(task) {
+      state.tasks = state.tasks.map((t) => {
+        if (t.name === task.name) {
+          return { ...t, isDone: !t.isDone };
+        }
+        return { ...t };
+      });
+    }
+    function remove(task) {
+      state.tasks = state.tasks.filter((t) => t.name !== task.name);
+      console.log("task", task);
+    }
+
+    return {
+      state,
+      handleShowHideList,
+      addTask,
+      complete,
+      remove
+    };
+  },
+};
+</script>
+
+<style scoped>
+.linha {
+  text-decoration: line-through;
+}
+</style> 
