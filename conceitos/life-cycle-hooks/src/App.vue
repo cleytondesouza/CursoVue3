0757b2ac5308ad4372a776bfe3d69c7550aee757
Cleyton Pereira de Souza <79112944+cleytondesouza@users.noreply.github.com>
2022-01-05 11:19:50 -0300

    Maquina rodando no docker

--- conceitos/life-cycle-hooks/src/App.vue
+++ conceitos/life-cycle-hooks/src/App.vue
@@ -16,6 +16,9 @@ export default {
 export default {
   mounted(){
     console.log('Componente foi montado')
+
+  ----Apos montar as máquinas e subir pra o docker temos que ter em mente que nosso back agora é somente didático
+  Ele nao tem conexão alguma com nenhum banco de dados
   }
 }
 */
