d8a2b3778af5eeec14e9229449554880db2b9907
Cleyton Pereira de Souza <79112944+cleytondesouza@users.noreply.github.com>
2022-01-06 12:56:13 -0300

    Criando Páginas e fazendo Router

--- treinamento-vue3-code/dashboard/src/views/Home/index.vue
+++ treinamento-vue3-code/dashboard/src/views/Home/index.vue
@@ -1,6 +1,36 @@
 <template>
-  <h1 class="font-black text-brand-main">Aqui é a Home</h1>
+  <custom-header @create-account="handleAcountCreate" @login="handleLogin" />
+  <contact />
+
+
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker 2021</p>
+  </div>
 </template>
+
 <script>
-export default {}
+import { oneMounted } from "vue";
+import { useRouter } from "vue-router";
+import CustonHeader from "./CustomHeader.vue";
+import Contact from "./Contact.vue";
+
+export default {
+  components: { CustonHeader, Contact },
+  setup() {
+    const router = useRouter();
+    oneMounted(() => {
+      const token = window.localStorage.getItem("token");
+      if (token) {
+        router.push({ name: "Feedbacks" });
+      }
+    });
+    function handleLogin() {}
+    function handleAcountCreate() {}
+    return {
+      handleLogin,
+      handleAcountCreate,
+    };
+  },
+};
 </script>
+ 
