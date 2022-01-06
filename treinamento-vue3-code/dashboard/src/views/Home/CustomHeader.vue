0615b50868e86d2a9b381d2a6946edee0c555bc5
Cleyton Pereira de Souza <79112944+cleytondesouza@users.noreply.github.com>
2022-01-06 17:31:24 -0300

    Criação de Classes e Mods da template

--- treinamento-vue3-code/dashboard/src/views/Home/CustomHeader.vue
+++ treinamento-vue3-code/dashboard/src/views/Home/CustomHeader.vue
@@ -1,7 +1,42 @@
 <template>
-  <h1>Meu Header</h1>
+  <header class="header">
+    <div class="flex flex-col w-4/5 max-w-6x1">
+      <div class="w-28 lg:w-36">
+        <img
+          class="w-full"
+          src="../../assets/images/logo_white.png"
+          alt="logo"
+        />
+      </div>
+      <div class="flex">
+        <button
+          class="px-6 py-2 font-bold rounded-full text-white focus:outline-none"
+        >
+          Crie uma Conta
+        </button>
+        <button
+          class="
+            px-6
+            py-2
+            font-bold
+            bg-white
+            rounded-full
+            text-brand-main
+            focus:outline-none
+          "
+        >
+          Entrar
+        </button>
+      </div>
+    </div>
+  </header>
 </template>
 <script>
 export default {}
 </script>
-
+<style lang="postcss" scoped>
+.header {
+  @apply bg-brand-main w-full flex justify-center;
+  height: 700px;
+}
+</style>
--- treinamento-vue3-code/dashboard/src/views/Home/index.vue
+++ treinamento-vue3-code/dashboard/src/views/Home/index.vue
@@ -1,36 +1,33 @@
 <template>
   <custom-header @create-account="handleAcountCreate" @login="handleLogin" />
   <contact />
-
-
   <div class="flex justify-center py-10 bg-brand-gray">
-    <p class="font-medium text-center text-gray-800">feedbacker 2021</p>
+    <p class="font-medium text-center text-gray-800">feedbacker © 2022</p>
   </div>
 </template>
 
 <script>
-import { oneMounted } from "vue";
-import { useRouter } from "vue-router";
-import CustonHeader from "./CustomHeader.vue";
-import Contact from "./Contact.vue";
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
 
 export default {
-  components: { CustonHeader, Contact },
-  setup() {
-    const router = useRouter();
-    oneMounted(() => {
-      const token = window.localStorage.getItem("token");
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
       if (token) {
-        router.push({ name: "Feedbacks" });
+        router.push({ name: 'Feedbacks' })
       }
-    });
-    function handleLogin() {}
-    function handleAcountCreate() {}
+    })
+    function handleLogin () { }
+    function handleAcountCreate () { }
     return {
       handleLogin,
-      handleAcountCreate,
-    };
-  },
-};
+      handleAcountCreate
+    }
+  }
+}
 </script>
- 
