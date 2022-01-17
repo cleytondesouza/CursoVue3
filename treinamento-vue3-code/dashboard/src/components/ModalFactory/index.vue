3738eb6281bbc8574bf08729d1d76b4d87eb6603
Cleyton Pereira de Souza <79112944+cleytondesouza@users.noreply.github.com>
2022-01-17 18:18:02 -0300

    Alterações nas consultas

--- treinamento-vue3-code/dashboard/src/components/ModalFactory/index.vue
+++ treinamento-vue3-code/dashboard/src/components/ModalFactory/index.vue
@@ -27,51 +27,7 @@
           "
         >
           <div class="flex flex-col px-12 py-10 bg-white">
-            <component :is="state.component" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </teleport>
-</template>
 
-<script>
-import {
-  reactive,
-  onMounted,
-  onBeforeUnmount,
-  defineAsyncComponent
-} from 'vue'
-import useModal from '../../hooks/useModal'
-
-const ModalLogin = defineAsyncComponent(() => import('../ModalLogin'))
-const DEFAULT_WIDTH = 'w-3/4 lg:w-1/3'
-
-export default {
-  components: {
-    ModalLogin
-  },
-  setup () {
-    const modal = useModal()
-    const state = reactive({
-      isActive: false,
-      component: {},
-      props: {},
-      width: DEFAULT_WIDTH
-    })
-
-    onMounted(() => {
-      modal.listen(handleModalToogle)
-    })
-
-    onBeforeUnmount(() => {
-      modal.off(handleModalToogle)
-    })
-
-    function handleModalToogle (payload) {
-      if (payload.status) {
-        state.component = payload.component
-        state.props = payload.props
         state.width = payload.width ?? DEFAULT_WIDTH
       } else {
         state.component = {}
--- treinamento-vue3-code/dashboard/src/services/auth.js
+++ treinamento-vue3-code/dashboard/src/services/auth.js
@@ -9,3 +9,4 @@ export default httpClient => ({
         }
     }
 })
+/* Sincronização dos dados do elementy */
