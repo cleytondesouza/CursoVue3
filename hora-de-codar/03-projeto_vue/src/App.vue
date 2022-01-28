7b4e3adc6c98faa75aa4d35fb05ecb26a2a596fa
Cleyton Pereira de Souza <79112944+cleytondesouza@users.noreply.github.com>
2022-01-28 18:49:09 -0300

    Hierarquia

--- hora-de-codar/03-projeto_vue/src/App.vue
+++ hora-de-codar/03-projeto_vue/src/App.vue
@@ -3,18 +3,21 @@
   <PrimeiroComponente />
   <p>O dado é {{testando}}</p>
   <life-cycle />
+  <pessoa />
   </div>
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
 import LifeCycle from './components/LifeCycle.vue'
+import Pessoa from './components/pessoa.vue'
 
 export default {
   name: 'App',
   components: {
     PrimeiroComponente,
-    LifeCycle
+    LifeCycle,
+    Pessoa
   },
   data(){
     return{
---
+++ hora-de-codar/03-projeto_vue/src/components/Info.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <p>Estou trabalhando no momento.</p>
+  <p>Utilizo as seguinte tecnologias</p>
+  <ul>
+  <li>PHP</li>
+  <li>Vue3</li>
+  <li>Python</li>
+  </ul>
+</div>
+  
+</template>
+
+<script>
+export default {
+name: 'Info'
+}
+</script>
