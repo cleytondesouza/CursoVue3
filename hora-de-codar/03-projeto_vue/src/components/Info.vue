a0d382b5251cfea64fac47f1b864d8e32cf51fcb
Cleyton Pereira de Souza <79112944+cleytondesouza@users.noreply.github.com>
2022-01-31 18:17:50 -0300

    Update Info.vue

--- hora-de-codar/03-projeto_vue/src/components/Info.vue
+++ hora-de-codar/03-projeto_vue/src/components/Info.vue
@@ -1,18 +1,27 @@
 <template>
 <div>
-  <p>Estou trabalhando no momento.</p>
+  <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+  <p v-else>Estou em Busca de Trabalho</p>
   <p>Utilizo as seguinte tecnologias</p>
   <ul>
   <li>PHP</li>
   <li>Vue3</li>
   <li>Python</li>
   </ul>
+  <p v-show="mostrar_email">Mande uma mensagem para {{ email }}</p>
 </div>
   
 </template>
 
 <script>
 export default {
-name: 'Info'
+name: 'Info',
+data() {
+  return{
+    esta_trabalhando: false,
+    mostrar_email: true,
+    email: 'cleyton@gmaial.com'
+  }
+}
 }
 </script>
