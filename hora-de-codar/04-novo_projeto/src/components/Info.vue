91d6f0409ba2b6a76f76a0e405ea5a60b23ab419
Cleyton Pereira de Souza <79112944+cleytondesouza@users.noreply.github.com>
2022-02-01 22:28:02 -0300

    Novo Projeto

---
+++ hora-de-codar/04-novo_projeto/src/components/Info.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+  <p v-else>Estou em Busca de Trabalho</p>
+  <p>Utilizo as seguinte tecnologias</p>
+  <ul>
+  <li>PHP</li>
+  <li>Vue3</li>
+  <li>Python</li>
+  </ul>
+  <p v-show="mostrar_email">Mande uma mensagem para {{ email }}</p>
+  <p>Para acessar o meu portifólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+  <Picture />
+</div>
+  
+</template>
+
+<script>
+import Picture from './Picture.vue'
+export default {
+name: 'Info',
+components: {
+  Picture
+},
+data() {
+  return{
+    esta_trabalhando: false,
+    mostrar_email: true,
+    email: 'cleyton@gmaial.com',
+    meu_link: 'https://google.com'
+  }
+}
+}
+</script>
---
+++ hora-de-codar/04-novo_projeto/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+  name: "LifeCycle",
+  data() {
+    return {
+      nome: "Ainda nao sei"
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.nome = "Cleyton";
+    }, 1000);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.nome = "Pedro";
+    }, 2000);
+  }
+}
+</script>
