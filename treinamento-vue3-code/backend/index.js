632a83f5723e5bf6c1963fb1d7f6f3a88f2cb331
Cleyton Pereira de Souza <79112944+cleytondesouza@users.noreply.github.com>
2022-02-25 22:56:08 -0300

    Update index.js

--- treinamento-vue3-code/backend/index.js
+++ treinamento-vue3-code/backend/index.js
@@ -45,3 +45,25 @@ app.listen(PORT, () => {
 })
 
 module.exports = app
+/*
+router.head('/apikey/exists', apiKeyHandler.checkIfApiKeyExists)
+router.post('/auth/register', usersHandler.create)
+router.post('/auth/login', authHandler.login)
+router.get('/users/me', authMiddleware, usersHandler.getLoggerUser)
+router.post('/users/me/apikey', authMiddleware, usersHandler.generateApiKey)
+router.get('/feedbacks', authMiddleware, feedbacksHandler.getFeedbacks)
+router.post('/feedbacks', feedbacksHandler.create)
+router.get('/feedbacks/summary', authMiddleware, feedbacksHandler.getSummary)
+app.use(router.routes())
+
+---- Quando o programa apresentar uma mensagem de erro na tela inicial 'Erro de Token'
+- A tela aparecerá um script de 5 segundos que irá redirecinar para a tela de login
+
+- Se antes dos 5 segundos for pressionado o botão F5 a tela será recarregada e o token será 
+reconstruido aleatoriamente.
+
+- Caso o sistema não consiga reconstruir o token o sistema fará um acesso ao banco de dados para que
+uma verificação de autentficação seja feita e se existe a possibilidade de relogar automaticamente, esse processo não poderá
+levar mais que 2 segundos
+
+*/
