330a10031b5299f3b904819b021e3e8a307bdb92
Cleyton Pereira de Souza <notyelcleyton@hotmail.com>
2022-04-27 10:10:06 -0300

    Input e data binding

---
+++ hora-de-codar/03-Input_e_data_binding/src/app.js
@@ -0,0 +1,18 @@
+const meusDados = {
+    data () {
+        return {
+            name: "",
+            idade: 30,
+            input_name: ""
+        }
+    },
+    methods: {
+        submitForm (e){
+            e.preventDefault();
+            console.log(this.input_name);
+            this.name = this.input_name
+        }
+    }
+}
+
+Vue.createApp(meusDados).mount("#app");
