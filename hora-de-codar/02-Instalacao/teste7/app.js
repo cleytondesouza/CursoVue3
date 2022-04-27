c2286582497ac7e9f8620d7d9a35024283e19f3f
Cleyton Pereira de Souza <notyelcleyton@hotmail.com>
2022-04-27 09:08:20 -0300

    Teste com CSS da aula 2

---
+++ hora-de-codar/02-Instalacao/teste7/app.js
@@ -0,0 +1,10 @@
+const testeSete = {
+    data () {
+        return {
+            nome: "Cleyton P Souza",
+            idade: 33
+        }
+    }
+}
+
+Vue.createApp(testeSete).mount("#teste")
---
+++ hora-de-codar/02-Instalacao/teste8/script.js
@@ -0,0 +1,11 @@
+const teste8 = {
+    data () {
+        return {
+            nome: "Cleyton de Souza",
+            idade: 33,
+            profissao: "Analista de Sistemas",
+            telefone: "33984094113"
+        }
+    }
+}
+Vue.createApp(teste8).mount("#teste")
